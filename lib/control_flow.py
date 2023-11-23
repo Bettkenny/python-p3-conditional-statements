dadf4aa00826c50fbc20cc7ad86697719ac62ccb
YOUR NAME <YOUR@EMAIL.com>
2023-11-23 14:06:15 +0300

    complete lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if username == "admin" or "ADMIN":
+        password == "12345"
+        return "Access granted"
+    else: 
+        return"Access denied"
     pass
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+        print = f"It's {response} out there!"
+
     pass
 
 def fizzbuzz(num):
